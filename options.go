159d47de0100dd36d115a2a749a89f08fb3f0fa5
steden <1470804@qq.com>
2023-07-16 22:48:49 +0800

    增加快速设置参数方法

--- options.go
+++ options.go
@@ -13,3 +13,15 @@ type Op struct {
 	ExpiryType eumExpiryType.Enum // 过期策略
 	Expiry     time.Duration      // 缓存失效时间
 }
+
+// AbsoluteExpiration 绝对时间，到期自动移除
+func (receiver *Op) AbsoluteExpiration(expiry time.Duration) {
+	receiver.ExpiryType = eumExpiryType.AbsoluteExpiration
+	receiver.Expiry = expiry
+}
+
+// SlidingExpiration 任意一次访问都会重置过期时间
+func (receiver *Op) SlidingExpiration(expiry time.Duration) {
+	receiver.ExpiryType = eumExpiryType.SlidingExpiration
+	receiver.Expiry = expiry
+}
